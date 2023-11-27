f63869084cf386a6b07506be8a70c9ebd59cdd26
owenbcoding <eoghankb@gmail.com>
2023-11-27 02:50:49 +0000

    added some data for order query in charts route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,5 +9,22 @@ Route::get('/', function () {
 });
 
 Route::get('/charts', function () {
-    return view('charts');
+    $thisYearOrders = Order::query()
+        ->whereYear('created_at', date('Y'))
+        ->selectRaw('month(created_at) as month')
+        ->selectRaw('count(*) as count')
+        ->groupBy('month')
+        ->orderBy('month')
+        ->pluck('count', 'month')
+        ->values()
+        ->toArray();
+
+    $lastYearOrders = Order::query()
+        ->whereYear('created_at', date('Y') -1)
+        ->groupByMonth();
+
+    return view('charts', [
+        'thisYearOrders' => $thisYearOrders,
+        'lastYearOrders' => $lastYearOrders,
+    ]);
 });
