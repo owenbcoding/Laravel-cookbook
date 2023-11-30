480fffc523c5dcdb5efda6abe445288e586cbde8
owenbcoding <eoghankb@gmail.com>
2023-11-30 01:13:48 +0000

    changed order factory from  5k to 1k

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(1000)->create();
-        \App\Models\Order::factory(1000)->create();
+        \App\Models\Order::factory(5000)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
