fc1d48105a67e103fe45a11ccdb7294a98a34a2c
owenbcoding <eoghankb@gmail.com>
2024-01-29 09:24:36 +0000

    created livewire component users count

---
+++ app/Http/Livewire/Stats/UsersCount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Stats;
+
+use Livewire\Component;
+
+class UsersCount extends Component
+{
+    public function render()
+    {
+        return view('livewire.stats.users-count');
+    }
+}
---
+++ resources/views/livewire/stats/users-count.blade.php
@@ -0,0 +1,8 @@
+<div class="flex justify-between item-center">
+    <h4 class="text-gray-500 font-medium">users</h4>
+    <select name="selectedDays" id="selectedDays" class="border bg-gray-100">
+        <option value="30">30 days</option>
+        <option value="30">60 days</option>
+        <option value="30">90 days</option>
+    </select>
+</div>
