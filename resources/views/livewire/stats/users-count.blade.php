d0f2a2f7f0a3f202d4ca7228347f3471acb93853
owenbcoding <eoghankb@gmail.com>
2024-01-31 08:27:52 +0000

    added wire Model and wire changed to select

--- resources/views/livewire/stats/users-count.blade.php
+++ resources/views/livewire/stats/users-count.blade.php
@@ -1,8 +1,10 @@
 <div class="flex justify-between item-center">
     <h4 class="text-gray-500 font-medium">users</h4>
-    <select name="selectedDays" id="selectedDays" class="border bg-gray-100">
+    <select name="selectedDays" id="selectedDays" class="border bg-gray-100" wire:model="selectedDays" wire:change="updateStat">
         <option value="30">30 days</option>
         <option value="30">60 days</option>
         <option value="30">90 days</option>
     </select>
 </div>
+
+
