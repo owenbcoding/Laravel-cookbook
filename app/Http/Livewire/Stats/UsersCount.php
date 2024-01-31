b666225ba4c6f87d6cff4b23a9aa105a061eb91a
owenbcoding <eoghankb@gmail.com>
2024-01-31 08:27:02 +0000

    created some logic to update stat

--- app/Http/Livewire/Stats/UsersCount.php
+++ app/Http/Livewire/Stats/UsersCount.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Livewire\Stats;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UsersCount extends Component
 {
+    public $selectedDays;
+    public $usersCount;
+
+    public function mount() {
+       
+        $this->updateStat();
+    }
+
+    public function updateStat() {
+        $this->usersCount = User::where('created_at', '>', now()->subDays($this->selectedDays))->count();
+    }
+
     public function render()
     {
         return view('livewire.stats.users-count');
