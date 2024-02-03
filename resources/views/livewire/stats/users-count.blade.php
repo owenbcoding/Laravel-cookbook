72e33cac981bb38000fd4ef08224a9c9dfb9934f
owenbcoding <eoghankb@gmail.com>
2024-02-03 15:52:33 +0000

    removed extra div causing some styling issues

--- resources/views/livewire/stats/users-count.blade.php
+++ resources/views/livewire/stats/users-count.blade.php
@@ -1,10 +1,12 @@
-<div class="flex justify-between item-center">
-    <h4 class="text-gray-500 font-medium">users</h4>
-    <select name="selectedDays" id="selectedDays" class="border bg-gray-100" wire:model="selectedDays" wire:change="updateStat">
-        <option value="30">30 days</option>
-        <option value="30">60 days</option>
-        <option value="30">90 days</option>
-    </select>
-</div>
+
+    <div class="flex justify-between items-center">
+        <h4 class="text-gray-500 font-medium">users</h4>
+        <select name="selectedDays" id="selectedDays" class="border bg-gray-100" wire:model="selectedDays" wire:change="updateStat">
+            <option value="30">30 days</option>
+            <option value="30">60 days</option>
+            <option value="30">90 days</option>
+        </select>
+    </div>
+    <div class="text3xl font-bold mt-4">{{$usersCount}}</div>
 
 
